d226b0dda7204923c9b3367c6905f762fd1f227b
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-07-19 21:57:39 +0530

    add google map into dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { google } from "google-maps";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements AfterViewInit {
+    title = 'angular-gmap';
+    @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+    map: google.maps.Map;
+    lat = 5.949090;
+    lng = 80.571989;
 
-  constructor() { }
+    coordinates = new google.maps.LatLng(7.0008544, 80.7645785);
 
-  ngOnInit(): void {
-  }
+    mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+    };
 
+    marker = new google.maps.Marker({
+      position: this.coordinates,
+      map: this.map,
+    });
+
+    constructor(private http: HttpClient) { }
+
+
+    ngAfterViewInit() {
+      this.mapInitializer();
+    }
+
+    mapInitializer() {
+      this.map = new google.maps.Map(this.gmap.nativeElement, 
+      this.mapOptions);
+      this.marker.setMap(this.map);
+  } 
 }
