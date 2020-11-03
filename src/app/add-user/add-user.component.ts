9e202bbfec2ecd19be69cfc806f648dbe315cc86
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-11-03 22:20:17 +0530

    try to create add user componenet

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { UserContact } from '../usercontact/usercontact.model';
+import { UsercontactService } from '../usercontact/usercontact.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { first } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
+  // constructor(private formBuilder: FormBuilder) {}
+  constructor(private router: Router, private userService: UsercontactService) { }
+  addForm: FormGroup;
+  @Output()
+  createUsercontact = new EventEmitter<UserContact>();
 
-  constructor() { }
+  emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  ngOnInit(): void {
+  ngOnInit() {
+  // this.addForm = this.formBuilder.group({
+  //     id: [],
+  //     email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
+  //     firstname: ['', Validators.required],
+  //     lastname: ['', Validators.required]
+  //   });
   }
 
+  isInvalid(name: string) {
+    const control = this.addForm.get(name);
+    return control.invalid && control.dirty;
+  }
+
+  isEmailInvalid(name: string) {
+    const control = this.addForm.get(name);
+    return control.invalid && control.dirty;
+  }
+
+  onSubmit() {
+    console.log("submit");
+    
+    this.userService.create(this.addForm.value);
+
+    this.router.navigate(['/list-user']);
+  }
 }
